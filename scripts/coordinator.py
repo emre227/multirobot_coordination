b33ee2327845d007923df2234f7b9e484de6c938
emre227 <emre.oencue@hsbi.de>
2023-12-31 16:39:18 +0100

    Anpassungen an Kommentaren

--- scripts/coordinator.py
+++ scripts/coordinator.py
@@ -6,29 +6,28 @@ import tf                   #tf Bibliothek um mit den Frames zu arbeiten
 import math                 #für die Berechnung des Abstandes
 
 
-## @package multirobot_coordination
-# Dokumentation für den coordinator.py
-#
 # Der coordinator.py fragt nach einem User-Input für die Zielkoordinate und speichert diesen dann ab.
 # Danach werden die Koordinaten der TurtleBots abgefragt und der Abstand zum Ziel wird berechnet. 
-# Aus einer Liste mit den Abständen wird der niedrigste ermittelt, der zugehörige TurtleBot
-# bekommt dann den Befehl sich zum Ziel zu bewegen.
+# Aus einer Liste mit den Abständen wird der niedrigste Index ermittelt, der zugehörige TurtleBot
+# bekommt dann die Koordinaten und bewegt sich daraufhin zum Ziel.
 
 if __name__ == '__main__':
 
     
     # Started die Node mit den Namen "coordinator"
-    # @param 'coordinator' Name der Node die erstellt wird
     rospy.init_node('coordinator') 
     # Erzeugt ein tf-listener Objekt um die Koordinatentransformation abzuhören            
     listener = tf.TransformListener()  
     # erzeugt Publisher um die Nachricht zum Topic "tb1/move_base_simple/goal"
     # zu senden, erwartet eine Nachricht vom Typ "PoseStamped"
-    # falls mehr TBots genutzt werden müssen hier weitere hinzugefügt werden
     tb1_pub = rospy.Publisher("tb1/move_base_simple/goal", PoseStamped, queue_size=10)  
     tb2_pub = rospy.Publisher("tb2/move_base_simple/goal", PoseStamped, queue_size=10)  
     tb3_pub = rospy.Publisher("tb3/move_base_simple/goal", PoseStamped, queue_size=10)
     tb4_pub = rospy.Publisher("tb4/move_base_simple/goal", PoseStamped, queue_size=10)
+    # falls mehr TBots genutzt werden müssen hier weitere hinzugefügt werden
+    # Beispiel für einen fünften TBot:
+    #tb5_pub = rospy.Publisher("tb5/move_base_simple/goal", PoseStamped, queue_size=10)
+
 
     # Führt die Schleife aus, solange die Node nicht beendet wurde
     while not rospy.is_shutdown():                 
@@ -51,17 +50,20 @@ if __name__ == '__main__':
         try:
             #Fragt die Koordinate von dem Baselink-Frame des Tbots im Bezug zum Map-Frame ab
             #und gibt als Rückgabewert zwei Listen mit translatorischen und rotatorischen Koordinaten
-            #Wenn Mehr TBOTs genutzt werden müssen hier neue hinzugefügt werden
+
             (tb1_trans,tb1_rot) = listener.lookupTransform('map', 'tb1/base_link', rospy.Time(0))                                                                                                
             (tb2_trans,tb2_rot) = listener.lookupTransform('map', 'tb2/base_link', rospy.Time(0)) 
             (tb3_trans,tb3_rot) = listener.lookupTransform('map', 'tb3/base_link', rospy.Time(0))
             (tb4_trans,tb4_rot) = listener.lookupTransform('map', 'tb4/base_link', rospy.Time(0))
+            #Wenn Mehr TBOTs genutzt werden müssen hier neue hinzugefügt werden
+            #Beispiel für einen fünften TBot:
+            #(tb5_trans,tb5_rot) = listener.lookupTransform('map', 'tb5/base_link', rospy.Time(0))           
         #Fängt Fehler ab, falls fehler mit den Frames auftreten
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):      
             continue
             
         
-        #Liste der translatorischen Koordinaten: wenn mehr TBots, auch hier hinzufügen
+        #Liste der translatorischen Koordinaten: wenn mehr TBots, dementsprechend Elemente zur Liste hinzufügen
         tb_trans_list = [tb1_trans, tb2_trans, tb3_trans, tb4_trans]              
 
         # index zum Hochzählen der Listen
@@ -79,8 +81,8 @@ if __name__ == '__main__':
         # der Index steht für den jeweiligen TBot
         dist_min_index = distance_list.index(min(distance_list))   
         # if-Bedingung, je nach Index wird der passende TBot ausgewählt
-        # 
-        # Falls mehr TBots, hier hinzufügen
+        # Index = 0 -> TBot1, Index = 1 -> TBot2, Index = 2 -> TBot3 usw.
+
         if dist_min_index == 0 : 
             # Sendet die Nachricht xy_goal an den zuvor festgelegten Topic               
             tb1_pub.publish(xy_goal)    
@@ -94,6 +96,11 @@ if __name__ == '__main__':
             print("TBot3 moves to X: ",x_goal," Y: ", y_goal)
         elif dist_min_index == 3:
             tb4_pub.publish(xy_goal)
-            print("TBot4 moves to X: ",x_goal," Y: ", y_goal)            
+            print("TBot4 moves to X: ",x_goal," Y: ", y_goal)   
+        # Falls mehr TBots, hier hinzufügen
+        #Beispiel für einen fünften TBot:
+        #elif dist_min_index == 4:
+        #    tb5_pub.publish(xy_goal)
+        #    print("TBot5 moves to X: ",x_goal," Y: ", y_goal)      
         else:
             pass
